82e7b79a5f3ba3e30a476fa15f0cb0325e4bc8f7
unknown <auliazykha@gmai.com>
2024-06-15 19:15:16 +0700

    style: [ZA] cust chart

--- src/main.js
+++ src/main.js
@@ -2,9 +2,22 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { createVuetify } from "vuetify";
+import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
+import "@mdi/font/css/materialdesignicons.css";
 
 loadFonts();
 
 createApp(App).use(router).use(store).use(vuetify).mount("#app");
+
+export default createVuetify({
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+});
